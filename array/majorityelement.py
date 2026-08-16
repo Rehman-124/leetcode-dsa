a474f52dd1403f7d6567c41541d8cf39b4c2b53b
rehman-124 <abdulrehman.iqbal1247@gmail.com>
2026-08-17 01:45:54 +0500

    Solve Majority Element

---
+++ array/majorityelement.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        candidate = nums[0]
+        count = 1
+
+        for num in nums[1:]:
+            if count == 0:
+                candidate = num
+                count = 1
+
+            elif num == candidate:
+                count += 1
+
+            else:
+                count -= 1
+
+        return candidate
